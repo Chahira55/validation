a37e46137ba54f6b5f86be520d55c6aca1774d99
ZIED443 <ziedjmal99@gmail.com>
2023-04-01 19:12:57 +0100

    first commit

---
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.Car;
+
+import com.example.demo.service.CarService;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public List<Car> findAllCars() {
+        return carService.findAllCars();
+    }
+
+    @GetMapping("/available")
+    public List<Car> findAvailableCars() {
+        return carService.findAvailableCars();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> findCarById(@PathVariable Long id) {
+        Optional<Car> optionalCar = carService.findCarById(id);
+        return optionalCar.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/{id}/rent")
+    public ResponseEntity<Void> rentCar(@PathVariable Long id) throws Exception {
+        carService.rentCar(id);
+        return ResponseEntity.ok().build();
+    }
+}
